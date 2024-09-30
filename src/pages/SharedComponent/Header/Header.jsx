42792b183c74eb181285a81010eef159f3987c5d
Nadim Mahmud Nion <nadim15-1746@diu.edu.bd>
2024-10-01 00:00:03 +0600

    header, react icon, wesbite name added in the html and svg added

--- src/pages/SharedComponent/Header/Header.jsx
+++ src/pages/SharedComponent/Header/Header.jsx
@@ -1,6 +1,14 @@
 import React from 'react';
+import { CgProfile } from 'react-icons/cg';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
+    const navItems = <>
+        <li className='text-lg text-black'>
+            <Link to="/">Stays</Link>
+        </li>
+    </>;
+
     return (
         <div className="navbar bg-base-100">
             <div className="navbar-start">
@@ -22,36 +30,23 @@ const Header = () => {
                     <ul
                         tabIndex={0}
                         className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow">
-                        <li><a>Item 1</a></li>
-                        <li>
-                            <a>Parent</a>
-                            <ul className="p-2">
-                                <li><a>Submenu 1</a></li>
-                                <li><a>Submenu 2</a></li>
-                            </ul>
-                        </li>
-                        <li><a>Item 3</a></li>
+                        {navItems}
                     </ul>
                 </div>
-                <a className="btn btn-ghost text-xl">daisyUI</a>
+                <a className="btn btn-ghost text-xl font-bold text-rose-500">Airbnb</a>
             </div>
             <div className="navbar-center hidden lg:flex">
                 <ul className="menu menu-horizontal px-1">
-                    <li><a>Item 1</a></li>
-                    <li>
-                        <details>
-                            <summary>Parent</summary>
-                            <ul className="p-2">
-                                <li><a>Submenu 1</a></li>
-                                <li><a>Submenu 2</a></li>
-                            </ul>
-                        </details>
-                    </li>
-                    <li><a>Item 3</a></li>
+                    {navItems}
                 </ul>
             </div>
             <div className="navbar-end">
-                <a className="btn">Button</a>
+                <ul>
+                    <li className='text-md text-black mr-2'>Airbnb your home</li>
+                </ul>
+                <button className="btn">
+                    <CgProfile className='text-4xl' />
+                </button>
             </div>
         </div>
     );
