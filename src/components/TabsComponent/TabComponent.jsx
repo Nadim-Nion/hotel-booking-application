47c22f0f77891fa369e1cbd068855c1ff745bec7
Nadim Mahmud Nion <nadim15-1746@diu.edu.bd>
2024-10-03 15:32:00 +0600

    tabs added and applied style on it

---
+++ src/components/TabsComponent/TabComponent.jsx
@@ -0,0 +1,33 @@
+import React, { useState } from 'react';
+import { BsFillSunsetFill } from 'react-icons/bs';
+import { FaStar, FaUmbrellaBeach } from 'react-icons/fa';
+import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
+import 'react-tabs/style/react-tabs.css';
+
+const TabComponent = () => {
+    const [tabIndex, setTabIndex] = useState(0);
+
+    return (
+        <Tabs className='bg-white rounded-lg shadow-md p-4 w-full max-w-5xl mx-auto' defaultIndex={tabIndex} onSelect={(index) => setTabIndex(index)}>
+            <TabList className="flex justify-center w-full md:w-auto mx-auto">
+                <Tab>
+                    <FaStar className='text-2xl' />
+                    Icons
+                </Tab>
+                <Tab>
+                    <FaUmbrellaBeach className='text-2xl' />
+                    Beach
+                </Tab>
+                <Tab>
+                    <BsFillSunsetFill className='text-2xl' />
+                    Amazing Views
+                </Tab>
+            </TabList>
+            <TabPanel></TabPanel>
+            <TabPanel></TabPanel>
+            <TabPanel></TabPanel>
+        </Tabs>
+    );
+};
+
+export default TabComponent;
--- src/pages/HomePage/Home/Home.jsx
+++ src/pages/HomePage/Home/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import SearchBar from '../../../components/SearchBar/SearchBar';
+import TabComponent from '../../../components/TabsComponent/TabComponent';
 
 const Home = () => {
     return (
         <div>
             <SearchBar></SearchBar>
+            <TabComponent></TabComponent>
         </div>
     );
 };
