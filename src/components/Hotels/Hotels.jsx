dfdd2d1143218df3b6dd7c86e32bbd3a37763194
Nadim Mahmud Nion <nadim15-1746@diu.edu.bd>
2024-10-03 22:54:43 +0600

    fetch hotels data using tanstack query

--- src/components/Hotels/Hotels.jsx
+++ src/components/Hotels/Hotels.jsx
@@ -1,8 +1,10 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import HotelCard from '../HotelCard/HotelCard';
+import { useQuery } from '@tanstack/react-query';
+import useAxiosPublic from '../../hooks/useAxiosPublic';
 
 const Hotels = () => {
-    const [hotels, setHotels] = useState([]);
+    /* const [hotels, setHotels] = useState([]);
     useEffect(() => {
         fetch('hotels.json')
             .then(res => res.json())
@@ -10,7 +12,17 @@ const Hotels = () => {
                 console.log(data);
                 setHotels(data);
             })
-    }, []);
+    }, []); */
+
+    const axiosPublic = useAxiosPublic();
+
+    const { data: hotels = [] } = useQuery({
+        queryKey: ['hotels'],
+        queryFn: async () => {
+            const res = await axiosPublic.get('/hotels');
+            return res.data;
+        }
+    })
 
     return (
         <div className='grid md:grid-cols-3 sm:grid-cols-1 gap-4 w-full md:w-4/5 mx-auto my-8'>
--- src/main.jsx
+++ src/main.jsx
@@ -4,13 +4,20 @@ import './index.css'
 import {
   RouterProvider,
 } from "react-router-dom";
+import {
+  QueryClient,
+  QueryClientProvider,
+} from '@tanstack/react-query';
 
 
 import router from './routes/BasicRoutes.jsx';
+const queryClient = new QueryClient();
 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+    </QueryClientProvider>
   </StrictMode>,
 )
