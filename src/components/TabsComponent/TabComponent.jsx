9553d1fd8468d3e8fc32c48521f676145df2c16a
Nadim Mahmud Nion <nadim15-1746@diu.edu.bd>
2024-10-03 15:38:21 +0600

    icons and text are horizontally and vertically centered

--- src/components/TabsComponent/TabComponent.jsx
+++ src/components/TabsComponent/TabComponent.jsx
@@ -10,15 +10,15 @@ const TabComponent = () => {
     return (
         <Tabs className='bg-white rounded-lg shadow-md p-4 w-full max-w-5xl mx-auto' defaultIndex={tabIndex} onSelect={(index) => setTabIndex(index)}>
             <TabList className="flex justify-center w-full md:w-auto mx-auto">
-                <Tab>
+                <Tab className="flex flex-col items-center justify-center mx-2 p-2">
                     <FaStar className='text-2xl' />
                     Icons
                 </Tab>
-                <Tab>
+                <Tab className="flex flex-col items-center justify-center mx-2 p-2">
                     <FaUmbrellaBeach className='text-2xl' />
                     Beach
                 </Tab>
-                <Tab>
+                <Tab className="flex flex-col items-center justify-center mx-2 p-2">
                     <BsFillSunsetFill className='text-2xl' />
                     Amazing Views
                 </Tab>
