1725aee9124c1d4c65df88674592c3a89828fa6f
Nadim Mahmud Nion <nadim15-1746@diu.edu.bd>
2024-10-03 22:01:59 +0600

    fixed the card and image height in hotel card

--- src/components/HotelCard/HotelCard.jsx
+++ src/components/HotelCard/HotelCard.jsx
@@ -4,13 +4,15 @@ const HotelCard = ({ hotel }) => {
     const { hotelName, hotelDestination, shortDescription, pricePerGuest, checkOut } = hotel;
     return (
         <div className='flex justify-center items-center'>
-            <div className="card bg-base-100 w-96 shadow-xl">
-                <figure>
+            <div className="card bg-base-100 w-96 shadow-xl h-[500px] flex flex-col">
+                <figure className='h-64'>
                     <img
                         src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
-                        alt="Shoes" />
+                        alt="Shoes"
+                        className='w-full h-full object-cover'
+                    />
                 </figure>
-                <div className="card-body">
+                <div className="card-body flex-1 flex flex-col justify-between">
                     <h2 className="card-title">
                         {hotelName} , {hotelDestination}
                     </h2>
