3422e687c41b8e29326b4aed11efa8d36631a43d
Nadim Mahmud Nion <nadim15-1746@diu.edu.bd>
2024-10-04 23:15:03 +0600

    swipper added in the card

--- src/components/HotelCard/HotelCard.jsx
+++ src/components/HotelCard/HotelCard.jsx
@@ -1,31 +1,24 @@
 import React from 'react';
 import { Swiper, SwiperSlide } from 'swiper/react';
+import { Pagination } from 'swiper/modules';
+
 import 'swiper/css';
-import 'swiper/css/navigation';
 import 'swiper/css/pagination';
-import { Navigation, Pagination, Mousewheel, Keyboard } from 'swiper/modules';
 
 const HotelCard = ({ hotel }) => {
     const { hotelName, hotelDestination, shortDescription, pricePerGuest, checkOut, images } = hotel;
     return (
         <div className='flex justify-center items-center'>
-            <div className="card bg-base-100 w-96 shadow-xl h-[500px] flex flex-col">
-                <figure className='h-64 w-full'>
+            <div className="card bg-base-100 w-full shadow-xl h-[500px] flex flex-col">
+                <figure className='h-64 w-full relative overflow-hidden'>
                     {/* <img
                         src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
                         alt="Shoes"
                         className='w-full h-full object-cover'
                     /> */}
-                    <Swiper
-                        cssMode={true}
-                        navigation={true}
-                        pagination={true}
-                        mousewheel={true}
-                        keyboard={true}
-                        modules={[Navigation, Pagination, Mousewheel, Keyboard]}
-                        className="mySwiper"
-                    >
-                        {/* <SwiperSlide>Slide 1</SwiperSlide>
+
+                    {/* <Swiper pagination={true} modules={[Pagination]} className="mySwiper">
+                        <SwiperSlide>Slide 1</SwiperSlide>
                         <SwiperSlide>Slide 2</SwiperSlide>
                         <SwiperSlide>Slide 3</SwiperSlide>
                         <SwiperSlide>Slide 4</SwiperSlide>
@@ -33,17 +26,19 @@ const HotelCard = ({ hotel }) => {
                         <SwiperSlide>Slide 6</SwiperSlide>
                         <SwiperSlide>Slide 7</SwiperSlide>
                         <SwiperSlide>Slide 8</SwiperSlide>
-                        <SwiperSlide>Slide 9</SwiperSlide> */}
+                        <SwiperSlide>Slide 9</SwiperSlide>
+                    </Swiper> */}
+                    <Swiper pagination={true} modules={[Pagination]} className="mySwiper" style={{ height: '100%' }} >
                         {
-                            images.map((image, index) => <SwiperSlide
-                                key={index}
-                            >
-                                <img
-                                    src={image}
-                                    alt={`Hotel Image ${index + 1}`}
-                                    className='w-full h-full object-cover'
-                                />
-                            </SwiperSlide>)
+                            images.map((image, index) => (
+                                <SwiperSlide key={index} className='h-full'>
+                                    <img
+                                        src={image}
+                                        alt={`Hotel Image ${index + 1}`}
+                                        className='w-full h-full object-cover'
+                                    />
+                                </SwiperSlide>
+                            ))
                         }
                     </Swiper>
                 </figure>
@@ -62,7 +57,7 @@ const HotelCard = ({ hotel }) => {
                     }
                 </div>
             </div>
-        </div>
+        </div >
     );
 };
 
